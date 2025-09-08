844131ea862e302f1b0c2d4101c9d4f77fac3cc5
System <noreply@github.com>
2025-09-08 23:50:28 +0100

    Auto-commit: 2025-09-08 23:50:28

--- modules/hm/desktop/hyprland-ghostty.nix
+++ modules/hm/desktop/hyprland-ghostty.nix
@@ -11,24 +11,19 @@ in
   };
 
   config = mkIf cfg.enable {
-    # Create an override config that will be sourced by Hyprland
-    home.file.".config/hypr/conf/ghostty-override.conf".text = ''
-      # Override terminal keybindings to use Ghostty instead of Kitty
-      # This unbinds the existing keybindings and creates new ones
+    # Directly override terminal keybindings in Hyprland settings
+    wayland.windowManager.hyprland.settings = {
+      # Unbind existing terminal keybindings
+      unbind = [
+        "SUPER, T"
+        "SUPER ALT, T"
+      ];
       
-      unbind = SUPER, T
-      unbind = SUPER ALT, T
-      
-      # Bind Super+T to open Ghostty
-      bind = SUPER, T, exec, ghostty
-      
-      # Bind Super+Alt+T to open floating Ghostty
-      bind = SUPER ALT, T, exec, [float; move 20% 5%; size 60% 60%] ghostty
-    '';
-    
-    # Ensure this config is sourced by adding it to the main hyprland.conf
-    wayland.windowManager.hyprland.extraConfig = mkAfter ''
-      source = ~/.config/hypr/conf/ghostty-override.conf
-    '';
+      # Bind new keybindings to use Ghostty instead of Kitty
+      bind = [
+        "SUPER, T, exec, ghostty"
+        "SUPER ALT, T, exec, [float; move 20% 5%; size 60% 60%] ghostty"
+      ];
+    };
   };
 }
