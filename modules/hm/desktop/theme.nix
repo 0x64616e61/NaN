62915f2658adf554ade5410aa39813af55308e7b
a <a@os.int>
2025-09-28 20:30:29 +0100

    Fix Hyprland group color property syntax - use dots instead of underscores

--- modules/hm/desktop/theme.nix
+++ modules/hm/desktop/theme.nix
@@ -19,10 +19,10 @@ in
       };
       
       group = {
-        "col.border_active" = "rgba(94e2d5ff) rgba(89dcebff) 45deg";
-        "col.border_inactive" = "rgba(313244cc) rgba(45475acc) 45deg";
-        "col.border_locked_active" = "rgba(94e2d5ff) rgba(89dcebff) 45deg";
-        "col.border_locked_inactive" = "rgba(313244cc) rgba(45475acc) 45deg";
+        "col.border.active" = "rgba(94e2d5ff) rgba(89dcebff) 45deg";
+        "col.border.inactive" = "rgba(313244cc) rgba(45475acc) 45deg";
+        "col.border.locked_active" = "rgba(94e2d5ff) rgba(89dcebff) 45deg";
+        "col.border.locked_inactive" = "rgba(313244cc) rgba(45475acc) 45deg";
       };
     };
   };
