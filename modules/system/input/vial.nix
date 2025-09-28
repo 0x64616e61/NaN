32fb0edf06df4a7fae022da7b648877717e62900
a <a@os.int>
2025-09-28 16:32:17 +0100

    Fix Vial udev rules syntax
    - Removed inline comments that caused syntax errors
    - Fixed comma separation in udev rules
    - Maintains all vendor ID coverage for Kyria and QMK keyboards
    - Resolves udev rules validation failure

--- modules/system/input/vial.nix
+++ modules/system/input/vial.nix
@@ -16,21 +16,21 @@ in
       vial
     ];
 
-    # Add udev rules for Vial keyboard access (Official Vial rules)
+    # Add udev rules for Vial keyboard access (Official Vial rules with proper syntax)
     services.udev.extraRules = ''
       # Official Universal Vial rule from get.vial.today
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
 
       # Kyria-specific vendor IDs (Pro Micro controllers)
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="1b4f", MODE="0660", GROUP="users", TAG+="uaccess"  # SparkFun Pro Micro
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2341", MODE="0660", GROUP="users", TAG+="uaccess"  # Arduino
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="239a", MODE="0660", GROUP="users", TAG+="uaccess"  # Adafruit
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1b4f", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2341", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="239a", MODE="0660", GROUP="users", TAG+="uaccess"
 
       # Additional QMK/VIA/Vial vendor IDs
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", MODE="0660", GROUP="users", TAG+="uaccess"  # QMK
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", MODE="0660", GROUP="users", TAG+="uaccess"  # Generic QMK
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", MODE="0660", GROUP="users", TAG+="uaccess"  # VUSB
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="04d8", MODE="0660", GROUP="users", TAG+="uaccess"  # Microchip
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="04d8", MODE="0660", GROUP="users", TAG+="uaccess"
 
       # Generalized rule for VIA/Vial keyboards without specific serial
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
