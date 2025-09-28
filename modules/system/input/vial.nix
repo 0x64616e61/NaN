8d2951faeabe321bf28e602f9377b8332c557ed6
a <a@os.int>
2025-09-28 16:22:56 +0100

    Enhanced Vial udev rules with Kyria support
    - Added official Vial udev rules from get.vial.today
    - Added Kyria-specific vendor IDs (Pro Micro controllers)
    - Added comprehensive QMK/VIA/Vial vendor support
    - Added bootloader detection for firmware flashing

--- modules/system/input/vial.nix
+++ modules/system/input/vial.nix
@@ -16,24 +16,34 @@ in
       vial
     ];
 
-    # Add udev rules for Vial keyboard access
+    # Add udev rules for Vial keyboard access (Official Vial rules)
     services.udev.extraRules = ''
-      # Universal Vial rule - works with all Vial-enabled keyboards
+      # Official Universal Vial rule from get.vial.today
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-      
+
+      # Kyria-specific vendor IDs (Pro Micro controllers)
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1b4f", MODE="0660", GROUP="users", TAG+="uaccess"  # SparkFun Pro Micro
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2341", MODE="0660", GROUP="users", TAG+="uaccess"  # Arduino
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="239a", MODE="0660", GROUP="users", TAG+="uaccess"  # Adafruit
+
+      # Additional QMK/VIA/Vial vendor IDs
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", MODE="0660", GROUP="users", TAG+="uaccess"  # QMK
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", MODE="0660", GROUP="users", TAG+="uaccess"  # Generic QMK
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", MODE="0660", GROUP="users", TAG+="uaccess"  # VUSB
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="04d8", MODE="0660", GROUP="users", TAG+="uaccess"  # Microchip
+
       # Generalized rule for VIA/Vial keyboards without specific serial
-      # This is more permissive but ensures compatibility
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-      
-      # Additional rule for keyboards that might not properly identify
-      # Covers most custom mechanical keyboards
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", MODE="0660", GROUP="users", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", MODE="0660", GROUP="users", TAG+="uaccess"
-      
-      # QMK DFU bootloader rules (for flashing)
+
+      # QMK DFU bootloader rules (for flashing firmware)
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", MODE="0660", GROUP="users", TAG+="uaccess"
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ffb", MODE="0660", GROUP="users", TAG+="uaccess"
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff0", MODE="0660", GROUP="users", TAG+="uaccess"
+
+      # Caterina bootloader (common in Pro Micro)
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b4f", ATTRS{idProduct}=="9203", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b4f", ATTRS{idProduct}=="9205", MODE="0660", GROUP="users", TAG+="uaccess"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", MODE="0660", GROUP="users", TAG+="uaccess"
     '';
 
     # Note: Ensure your user is in the 'users' group in configuration.nix
