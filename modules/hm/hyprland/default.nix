14ffc6be4d7611c4cf5555b59d97f3b29d197eb2
a <a@os.int>
2025-09-28 20:38:33 +0100

    Add GPD Pocket 3 function key bindings for volume, brightness and media

--- modules/hm/hyprland/default.nix
+++ modules/hm/hyprland/default.nix
@@ -140,6 +140,23 @@ in
           # dmenu launcher
           "$mainMod, SPACE, exec, ${pkgs.dmenu-wayland}/bin/dmenu-wl_run -b -p 'Run: ' -fn 'monospace:size=10' -nb '#1a1a1a' -nf '#cccccc' -sb '#333333' -sf '#ffffff' -h 25"
           
+          # Function keys for GPD Pocket 3
+          # Volume controls
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          
+          # Brightness controls
+          ", XF86MonBrightnessUp, exec, brightnessctl set 10%+"
+          ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
+          
+          # Media controls
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPrev, exec, playerctl previous"
+          
           # Move focus
           "$mainMod, left, movefocus, l"
           "$mainMod, right, movefocus, r"
